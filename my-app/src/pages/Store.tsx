ca9bdc1cb4a609b8e869764f395afd141d433d5c
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-10-21 13:27:15 +0200

    Added Table component in store app

--- my-app/src/pages/Store.tsx
+++ my-app/src/pages/Store.tsx
@@ -1,5 +1,10 @@
+import ProductsTable from "../components/Table/ProductsTable";
+import { useGetProductsQuery } from "../API/api";
+
 const Store: React.FC = () => {
-  return <h1>Store</h1>;
+  const { data: products, isFetching } = useGetProductsQuery();
+
+  return <ProductsTable products={products ? products.data : []} />;
 };
 
 export default Store;
