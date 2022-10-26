52ed423e81a7c8afb924dce6361e70c0eb772ad2
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-10-26 15:34:56 +0200

    Added dropdown container and implemented onclick event

--- my-app/src/components/DropDown/DropDown.tsx
+++ my-app/src/components/DropDown/DropDown.tsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import Icon from "../Icon/Icon";
 import DownArrow from "../../imgs/down-arrow.svg";
+import { useState } from "react";
 
 export type DropDownProps = {
   options: string[];
   label: string;
+  isDropDownOpen: boolean;
+  onClick: () => void;
+  onSelect: (category: string) => void;
 };
 
 const StyledButton = styled.button`
@@ -19,18 +23,50 @@ const StyledButton = styled.button`
   font-size: 13px;
 `;
 
-const LeftContainer = styled.div``;
-const RightContainer = styled.div``;
+const DropDownContainer = styled.div`
+  height: 170px;
+  width: 300px;
+  background: rgba(0, 0, 0, 0.7);
+  z-index: 4;
+  border-radius: 10px;
+  padding: 3px;
+  text-overflow: scroll;
+  position: absolute;
+  top: 290px; ;
+`;
+const DropDownItem = styled.div`
+  background: grey;
+  margin-top: 2px;
+  border-radius: 10px;
+  padding: 5px;
+  font-size: 13px;
+  color: white;
+`;
 
-const DropDown: React.FC<DropDownProps> = ({ options, label }) => {
+const DropDown: React.FC<DropDownProps> = ({
+  options,
+  label,
+  isDropDownOpen,
+  onClick,
+  onSelect,
+}) => {
   return (
     <div>
-      <StyledButton>
-        <LeftContainer>{label}</LeftContainer>
-        <RightContainer>
+      <StyledButton onClick={onClick}>
+        <div>{label}</div>
+        <div>
           <Icon imageURL={DownArrow}></Icon>
-        </RightContainer>
+        </div>
       </StyledButton>
+      {isDropDownOpen && (
+        <DropDownContainer>
+          {options.map((item, key) => (
+            <DropDownItem key={key} onClick={() => onSelect(item)}>
+              {item}
+            </DropDownItem>
+          ))}
+        </DropDownContainer>
+      )}
     </div>
   );
 };
