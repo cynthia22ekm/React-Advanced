5936c79701b3cfa66e485453e0b0b9a8dbcc25c4
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-10-27 15:23:50 +0200

    Added Navbar to store

--- my-app/src/pages/Store/Store.tsx
+++ my-app/src/pages/Store/Store.tsx
@@ -1,9 +1,29 @@
+import { useCallback, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Products } from "../../Data/DataType";
 import StoreItem from "./StoreItem/StoreItem";
 
 const Store: React.FC = () => {
+  const [itemCount, setItemCount] = useState(0);
+  const addToCartHandler = useCallback(
+    (item: Products) => {
+      setItemCount(itemCount + 1);
+    },
+    [itemCount, setItemCount]
+  );
+
   return (
     <div>
-      <StoreItem></StoreItem>
+      <Navbar
+        linkNames={[
+          { linkName: "Home", linkPath: "/" },
+          { linkName: "Store", linkPath: "/store" },
+          { linkName: "About", linkPath: "/about" },
+        ]}
+        searchable={true}
+        itemCount={itemCount}
+      />
+      <StoreItem onAdd={addToCartHandler}></StoreItem>
     </div>
   );
 };
