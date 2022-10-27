5988c5c141ce3088098bfd3a9ee2921e2ebc2283
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-10-27 15:23:32 +0200

    Added an additional property to navbar

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import Navbar from "../components/Navbar/Navbar";
-import ProgressBar from "../components/ProgressBar/ProgressBar";
 
 const Home: React.FC = () => {
   return (
@@ -10,6 +9,7 @@ const Home: React.FC = () => {
         { linkName: "About", linkPath: "/about" },
       ]}
       searchable={true}
+      itemCount={0}
     />
   );
 };
