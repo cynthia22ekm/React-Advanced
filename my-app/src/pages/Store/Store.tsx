d3214e5bc51a0715bcd2fb5ee585c175755616a7
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-10-28 17:52:39 +0200

    Added shopping cart to store

--- my-app/src/pages/Store/Store.tsx
+++ my-app/src/pages/Store/Store.tsx
@@ -1,15 +1,50 @@
 import { useCallback, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Products } from "../../Data/DataType";
+import ShoppingCart from "./ShoppingCart/ShoppingCart";
 import StoreItem from "./StoreItem/StoreItem";
+
+export type CartItemType = {
+  id: number;
+  title: string;
+  quantity: number;
+  image: string;
+};
 
 const Store: React.FC = () => {
   const [itemCount, setItemCount] = useState(0);
+  const [cartItems, setCartItems] = useState<CartItemType[]>([]);
+
   const addToCartHandler = useCallback(
     (item: Products) => {
       setItemCount(itemCount + 1);
+      cartItems.length
+        ? cartItems.filter((cartItem) => cartItem.id === item.id).length
+          ? setCartItems(
+              cartItems.map((cartItem) => {
+                if (cartItem.id === item.id) {
+                  return {
+                    ...cartItem,
+                    quantity: cartItem.quantity + 1,
+                  };
+                } else return cartItem;
+              })
+            )
+          : setCartItems([
+              ...cartItems,
+              {
+                id: item.id,
+                title: item.title,
+                quantity: 1,
+                image: item.image,
+              },
+            ])
+        : setCartItems([
+            ...cartItems,
+            { id: item.id, title: item.title, quantity: 1, image: item.image },
+          ]);
     },
-    [itemCount, setItemCount]
+    [itemCount, cartItems, setItemCount, setCartItems]
   );
 
   return (
@@ -20,9 +55,13 @@ const Store: React.FC = () => {
           { linkName: "Store", linkPath: "/store" },
           { linkName: "About", linkPath: "/about" },
         ]}
-        searchable={true}
         itemCount={itemCount}
-      />
+      >
+        <ShoppingCart
+          itemCount={itemCount}
+          cartItems={cartItems}
+        ></ShoppingCart>
+      </Navbar>
       <StoreItem onAdd={addToCartHandler}></StoreItem>
     </div>
   );
