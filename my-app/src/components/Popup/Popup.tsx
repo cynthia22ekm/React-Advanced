4099a0ec5153968c403779b59be41865d6405017
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-10-28 17:51:14 +0200

    Created popup using usePopper

--- my-app/src/components/Popup/Popup.tsx
+++ my-app/src/components/Popup/Popup.tsx
@@ -1,186 +1,42 @@
-import { ChangeEvent, FormEvent, useCallback, useState } from "react";
+import { PositioningStrategy } from "@popperjs/core";
+import { PropsWithChildren, useState } from "react";
+import { PopperProps, usePopper } from "react-popper";
 import styled from "styled-components";
-import { ItemCategory } from "../../Data/DataType";
-import DropDown from "../DropDown/DropDown";
-import TextArea from "../TextArea/TextArea";
-import TextInput from "../TextInput/TextInput";
 
 export type PopupProps = {
-  imageID: number;
-  imageURL: string;
-  imageTitle: string;
-  imageCategory: ItemCategory;
-  imageDescription: string;
-  onClose: () => void;
-  onSubmit: (
-    id: number,
-    title: string,
-    category: ItemCategory,
-    description: string
-  ) => void;
-  onDelete: (imageID: number) => void;
+  referenceElement: HTMLButtonElement | null;
+  placement: PopperProps<never>["placement"];
+  strategy: PositioningStrategy;
 };
-
-let optionsValue: ItemCategory[] = [
-  "Food",
-  "Books",
-  "Clothing",
-  "Electronics",
-  "Home",
-];
 
 const StyledPopup = styled.div`
   z-index: 999;
-  position: fixed;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-`;
-
-const FullContent = styled.form`
-  z-index: 999;
-  display: block;
-  width: 550px;
-  height: 350px;
-  background: white;
-  margin-left: 400px;
-`;
-
-const StyledImage = styled.img`
-  width: 150px;
-  height: 150px;
-  padding: 10px;
-  margin-left: 20px;
-  margin-top: 20px;
-`;
-
-const StyledButton = styled.button`
-  margin-left: 10px;
-  margin-bottom: 10px;
-  border-radius: 5px;
-  border: none;
   padding: 5px;
-  width: 70px;
-  font-size: 13px;
+  border-radius: 10px;
+  background: rgba(0, 0, 0, 0.7);
 `;
 
-const StyledSaveButton = styled.button`
-  margin-left: 10px;
-  margin-bottom: 10px;
-  border-radius: 5px;
-  border: none;
-  padding: 5px;
-  width: 70px;
-  background: blue;
-  font-size: 13px;
-`;
-
-const ImageContent = styled.div`
-  padding: 20px;
-  display: block;
-`;
-
-const PopupBody = styled.div`
-  display: flex;
-`;
-
-const PopupFooter = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const LeftFooter = styled.div`
-  margin-left: 50px;
-`;
-
-const RightFooter = styled.div`
-  display: flex;
-  margin-right: 55px;
-`;
-
-const Popup: React.FC<PopupProps> = ({
-  imageID,
-  imageURL,
-  imageTitle,
-  imageCategory,
-  imageDescription,
-  onClose,
-  onSubmit,
-  onDelete,
+const Popup: React.FC<PropsWithChildren<PopupProps>> = ({
+  children,
+  referenceElement,
+  placement,
+  strategy,
 }) => {
-  const [isDropDownOpen, setDropDownOpen] = useState(false);
-  const [category, setCategory] = useState(imageCategory);
-  const [title, setTitle] = useState(imageTitle);
-  const [description, setDescription] = useState(imageDescription);
-
-  const openDropDownContainerHandler = useCallback(() => {
-    isDropDownOpen ? setDropDownOpen(false) : setDropDownOpen(true);
-  }, [setDropDownOpen, isDropDownOpen]);
-
-  const selectDropDownHandler = useCallback(
-    (category: ItemCategory) => {
-      setCategory(category);
-      setDropDownOpen(false);
-    },
-    [setCategory, setDropDownOpen]
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
+    null
   );
-
-  const getInputTextHandler = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      setTitle(event.target.value);
-    },
-    [setTitle]
-  );
-
-  const getTextAreaInputHandler = useCallback(
-    (event: ChangeEvent<HTMLTextAreaElement>) => {
-      setDescription(event.target.value);
-    },
-    [setDescription]
-  );
-
-  const formSubmitHandler = useCallback(
-    (event: FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      console.log("Hi");
-      onSubmit(imageID, title, category, description);
-    },
-    [imageID, title, category, description, onSubmit]
-  );
-
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: placement,
+    strategy: strategy,
+  });
+  console.log();
   return (
-    <StyledPopup>
-      <FullContent onSubmit={formSubmitHandler}>
-        <PopupBody>
-          <StyledImage src={imageURL} alt="image" />
-          <ImageContent>
-            <TextInput value={title} onChange={getInputTextHandler} />
-            <DropDown
-              label={category}
-              options={optionsValue}
-              isDropDownOpen={isDropDownOpen}
-              onClick={openDropDownContainerHandler}
-              onSelect={selectDropDownHandler}
-            />
-            <TextArea text={description} onChange={getTextAreaInputHandler} />
-          </ImageContent>
-        </PopupBody>
-        <PopupFooter>
-          <LeftFooter>
-            <StyledButton onClick={() => onDelete(imageID)}>
-              Delete
-            </StyledButton>
-          </LeftFooter>
-          <RightFooter>
-            <StyledButton onClick={onClose}>Cancel</StyledButton>
-            <StyledSaveButton type="submit">Save</StyledSaveButton>
-          </RightFooter>
-        </PopupFooter>
-      </FullContent>
+    <StyledPopup
+      style={styles.popper}
+      ref={setPopperElement}
+      {...attributes.popper}
+    >
+      {children}
     </StyledPopup>
   );
 };
