b193e8ad8b31a6f6f6b790ec50e48b73b45e9845
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-10-28 17:54:21 +0200

    Created view shopping cart popup

---
+++ my-app/src/pages/Store/ShoppingCart/ViewShoppingCart.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import Popup from "../../../components/Popup/Popup";
+import { CartItemType } from "../Store";
+
+export type ViewShoppingCartProps = {
+  cartItems: CartItemType[];
+  cartButtonReference: HTMLButtonElement | null;
+};
+
+const CartContainer = styled.div`
+  background: grey;
+  width: 300px;
+  height: 300px;
+  margin: 10px;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    width: 5px;
+    background: white;
+    border-radius: 10px;
+  }
+`;
+
+const CartItem = styled.div`
+  display: flex;
+`;
+
+const CartInfo = styled.div`
+  margin: 30px;
+  font-size: 13px;
+`;
+
+const StyledImage = styled.img`
+  width: 100px;
+  height: 100px;
+  margin: 20px;
+`;
+
+const ViewShoppingCart: React.FC<ViewShoppingCartProps> = ({
+  cartItems,
+  cartButtonReference,
+}) => {
+  return (
+    <Popup
+      placement="bottom-start"
+      strategy="fixed"
+      referenceElement={cartButtonReference}
+    >
+      <CartContainer>
+        {cartItems.map((item) => (
+          <CartItem>
+            <StyledImage src={item.image} alt="No Image" />
+            <div>
+              <CartInfo>{item.title}</CartInfo>
+              <CartInfo>Quantity:{item.quantity}</CartInfo>
+            </div>
+          </CartItem>
+        ))}
+      </CartContainer>
+    </Popup>
+  );
+};
+
+export default ViewShoppingCart;
