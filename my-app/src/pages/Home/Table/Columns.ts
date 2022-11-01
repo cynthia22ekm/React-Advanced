e8fd677570669782dece82a56ee99c170f10969f
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-11-01 18:33:45 +0100

    Created product table using useTable

---
+++ my-app/src/pages/Home/Table/Columns.ts
@@ -0,0 +1,30 @@
+import { Column } from "react-table";
+import { Products } from "../../../API/api";
+
+export const columns: Column<Products>[] = [
+  {
+    accessor: "category",
+    Header: "Category",
+    Footer: "Category",
+  },
+  {
+    accessor: "id",
+    Header: "Id",
+    Footer: "Id",
+  },
+  {
+    accessor: "image",
+    Header: "Image",
+    Footer: "Image",
+  },
+  {
+    accessor: "price",
+    Header: "Price",
+    Footer: "Price",
+  },
+  {
+    accessor: "title",
+    Header: "Title",
+    Footer: "Title",
+  },
+];
