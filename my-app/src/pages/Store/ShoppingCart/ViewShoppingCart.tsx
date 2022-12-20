c28812b3fe7e255be3928bd4f51a9348afc7eba7
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-20 11:22:38 +0100

    Implemented removeItem from shopping cart

--- my-app/src/pages/Store/ShoppingCart/ViewShoppingCart.tsx
+++ my-app/src/pages/Store/ShoppingCart/ViewShoppingCart.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import Popup from "../../../components/Popup/Popup";
 import { CartItemType } from "../Store";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 export type ViewShoppingCartProps = {
-  cartItems?: CartItemType[];
+  cartItems: CartItemType[];
   cartButtonReference: HTMLButtonElement | null;
 };
 
@@ -13,6 +13,8 @@ const CartContainer = styled.div`
   width: 300px;
   height: 300px;
   margin: 10px;
+  box-sizing: border-box;
+  padding: 10px;
   overflow-y: scroll;
   ::-webkit-scrollbar {
     width: 5px;
@@ -23,35 +25,43 @@ const CartContainer = styled.div`
 
 const CartItem = styled.div`
   display: flex;
+  box-sizing: border-box;
+  width: 275px;
 `;
 
 const CartInfo = styled.div`
-  margin: 30px;
+  margin-top: 20px;
   font-size: 13px;
 `;
 
 const StyledImage = styled.img`
   width: 100px;
   height: 100px;
-  margin: 20px;
+  margin: 10px;
 `;
 const StyledButton = styled.button`
-  margin-left: 10px;
-  margin-bottom: 10px;
   border-radius: 5px;
   border: none;
   padding: 5px;
-  width: 70px;
+  height: 30px;
+  width: 60px;
   font-size: 13px;
+  margin-top: 20px;
+  margin-left: 10px;
 `;
 
 const ViewShoppingCart: React.FC<ViewShoppingCartProps> = ({
   cartItems,
   cartButtonReference,
 }) => {
-  const removeItemHanlder = useCallback(() => {
-    console.log("Remove Item");
-  }, []);
+  const [cartData, setCartData] = useState<CartItemType[]>(cartItems);
+
+  const removeItemHanlder = useCallback(
+    (item: CartItemType) => {
+      setCartData(cartData.filter((eachData) => eachData.id !== item.id));
+    },
+    [cartData]
+  );
 
   return (
     <Popup
@@ -61,14 +71,16 @@ const ViewShoppingCart: React.FC<ViewShoppingCartProps> = ({
     >
       <CartContainer>
         {cartItems &&
-          cartItems.map((item) => (
+          cartData.map((item) => (
             <CartItem>
               <StyledImage src={item.image} alt="No Image" />
               <div>
                 <CartInfo>{item.title}</CartInfo>
                 <CartInfo>Quantity:{item.quantity}</CartInfo>
               </div>
-              <StyledButton onClick={removeItemHanlder}>Remove</StyledButton>
+              <StyledButton onClick={() => removeItemHanlder(item)}>
+                Remove
+              </StyledButton>
             </CartItem>
           ))}
       </CartContainer>
