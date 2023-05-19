00fa7c32fe7221317991300b66a2847ad2e1170b
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-05-19 11:30:06 +0200

    Created ABout page

--- my-app/src/pages/About/About.tsx
+++ my-app/src/pages/About/About.tsx
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import Navbar from "../../components/Navbar/Navbar";
+import { Contact } from "./Contact";
+import { ContactDetails } from "./ContactDetails";
+
+const ContactSection = styled.div`
+  margin-top: 20%;
+  margin-left: 30%;
+  padding: 10px;
+  width: 500px;
+  height: 500px;
+  background-color: lightblue;
+`;
+
 const About: React.FC = () => {
-  return <h1>About</h1>;
+  const onSubmitHandler = (data: ContactDetails) => console.log(data);
+  return (
+    <div>
+      <Navbar
+        linkNames={[
+          { linkName: "Home", linkPath: "/" },
+          { linkName: "Store", linkPath: "/store" },
+          { linkName: "About", linkPath: "/about" },
+        ]}
+        itemCount={0}
+      ></Navbar>
+      <ContactSection>
+        <Contact onSubmit={onSubmitHandler}></Contact>
+      </ContactSection>
+    </div>
+  );
 };
 
 export default About;
