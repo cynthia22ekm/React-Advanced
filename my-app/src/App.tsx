24142dd2d26c71dc07cced02772417faca6189a8
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-05-19 10:10:43 +0200

    Added Route for login

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import { productsApi } from "./API/api";
-import logo from "./logo.svg";
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Home from "./pages/Home/Home";
 import Store from "./pages/Store/Store";
-import About from "./pages/About";
+import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <ApiProvider api={productsApi}>
       <Container>
         <Routes>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="/" element={<Home />}></Route>
           <Route path="/store" element={<Store />}></Route>
           <Route path="/about" element={<About />}></Route>
