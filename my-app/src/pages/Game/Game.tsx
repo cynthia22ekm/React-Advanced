0e6385dbd56f67c5df72e9c564abd29db4a97052
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-05-31 17:59:05 +0200

    Created a game component

---
+++ my-app/src/pages/Game/Game.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+export type CoordinateType = {
+  x: number;
+  y: number;
+};
+
+const Game: React.FC = () => {
+  const [coordinates, setCoordinates] = useState<CoordinateType[]>([]);
+  const addCircleHandler = (event: React.MouseEvent<HTMLDivElement>) => {
+    console.log(event);
+    setCoordinates((previousCoordinates) => [
+      ...previousCoordinates,
+      { x: event.clientX, y: event.clientY },
+    ]);
+  };
+  return (
+    <div onClick={addCircleHandler} style={{ height: "100vh" }}>
+      {coordinates.map((coordinate, key) => (
+        <div
+          key={key}
+          style={{
+            borderRadius: "50%",
+            width: "50px",
+            height: "50px",
+            backgroundColor: "blue",
+            position: "fixed",
+            left: `${coordinate.x}px`,
+            top: `${coordinate.y}px`,
+          }}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
+export default Game;
