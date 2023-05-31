a58912a0157cc5f1d59399a94b9e8f87f2a74f58
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-05-31 18:00:00 +0200

    Added provider aroung main app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { Provider } from "react-redux";
 import { productsApi } from "./API/api";
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
@@ -7,19 +8,24 @@ import Home from "./pages/Home/Home";
 import Store from "./pages/Store/Store";
 import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
+import Game from "./pages/Game/Game";
+import { store } from "./store/store";
 
 function App() {
   return (
-    <ApiProvider api={productsApi}>
-      <Container>
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/store" element={<Store />}></Route>
-          <Route path="/about" element={<About />}></Route>
-        </Routes>
-      </Container>
-    </ApiProvider>
+    <Provider store={store}>
+      <ApiProvider api={productsApi}>
+        <Container>
+          <Routes>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/game" element={<Game />}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/store" element={<Store />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </Container>
+      </ApiProvider>
+    </Provider>
   );
 }
 
