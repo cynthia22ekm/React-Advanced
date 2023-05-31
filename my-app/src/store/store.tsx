e382ef5b0f27858690c5ace648c410646c1e061b
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-05-31 18:35:31 +0200

    Configured redux store and added reducer

--- my-app/src/store/store.tsx
+++ my-app/src/store/store.tsx
@@ -1,3 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import CounterReducer from "../reduxSlice/CounterSlice";
 
-export const Store = configureStore({ reducer: {} });
+export const Store = configureStore({
+  reducer: {
+    counter: CounterReducer,
+  },
+});
