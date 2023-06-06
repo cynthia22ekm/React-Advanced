90b9de56a9e5f89cd14e1f5f122cbb1a58ad75a5
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-06 16:38:34 +0200

    Created redux store

---
+++ my-app/src/store/ReduxStore.tsx
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import CounterReducer from "../reduxSlice/CounterSlice";
+import ShoppingCartReducer from "../reduxSlice/ShoppingCarSlice";
+import productsApi from "../API/api";
+
+//https://dev.to/ifeanyichima/what-is-createasyncthunk-in-redux--mhe
+
+export const ReduxStore = configureStore({
+  reducer: {
+    counter: CounterReducer,
+    shoppingcart: ShoppingCartReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+});
