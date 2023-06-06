f80938a4b7975863c1f4af220fd2285093f08c1b
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-06 17:53:49 +0200

    Removed ApiProvider

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,29 +1,26 @@
 import React from "react";
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import { Provider } from "react-redux";
-import { productsApi } from "./API/api";
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Home from "./pages/Home/Home";
-import Store from "./pages/Store/Store";
 import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
 import Game from "./pages/Game/Game";
+import { ReduxStore } from "./store/ReduxStore";
+import Store from "./pages/Store/Store";
 
 function App() {
   return (
-    <Provider store={Store}>
-      <ApiProvider api={productsApi}>
-        <Container>
-          <Routes>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/game" element={<Game />}></Route>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/store" element={<Store />}></Route>
-            <Route path="/about" element={<About />}></Route>
-          </Routes>
-        </Container>
-      </ApiProvider>
+    <Provider store={ReduxStore}>
+      <Container>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/game" element={<Game />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
     </Provider>
   );
 }
