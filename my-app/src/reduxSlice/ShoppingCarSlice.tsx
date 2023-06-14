edc6470933b2f92d316b5cd513532c2dae159591
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-14 17:35:33 +0200

    Added addMoreQuantity reducer

--- my-app/src/reduxSlice/ShoppingCarSlice.tsx
+++ my-app/src/reduxSlice/ShoppingCarSlice.tsx
@@ -38,12 +38,28 @@ export const ShoppingCartSlice = createSlice({
           });
     },
     removeFromCart: (state, action: PayloadAction<CartItemType>) => {
-      state.cartItems = state.cartItems.filter(
-        (cartItem) => cartItem.id !== action.payload.id
-      );
+      if (action.payload.quantity > 1) {
+        state.cartItems.map((cartItem) => {
+          if (cartItem.id === action.payload.id) {
+            cartItem.quantity = cartItem.quantity - 1;
+          }
+        });
+      } else if (action.payload.quantity === 1) {
+        state.cartItems = state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        );
+      }
+    },
+    addMoreQuantity: (state, action: PayloadAction<CartItemType>) => {
+      state.cartItems.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          cartItem.quantity = cartItem.quantity + 1;
+        }
+      });
     },
   },
 });
 
-export const { addToCart, removeFromCart } = ShoppingCartSlice.actions;
+export const { addToCart, removeFromCart, addMoreQuantity } =
+  ShoppingCartSlice.actions;
 export default ShoppingCartSlice.reducer;
