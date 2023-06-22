d8367eef8efe01c5ab5e8a82e76b05418dbf87a1
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-22 09:19:13 +0200

    Updated the select component and added ref

--- my-app/src/components/Select/Select.tsx
+++ my-app/src/components/Select/Select.tsx
@@ -1,9 +1,16 @@
-import Select from "react-select";
+import { ForwardedRef, forwardRef } from "react";
+import Select, { SelectInstance } from "react-select";
 
 export type SelectProps = {
   options: { value: string; label: string }[];
+  ref?: ForwardedRef<SelectInstance>;
 };
 
-const SelectBox: React.FC<SelectProps> = ({ options }) => {
-  return <Select options={options}></Select>;
-};
+const SelectBox: React.FC<SelectProps> = forwardRef<
+  SelectInstance,
+  SelectProps
+>(({ options, ...props }, ref) => {
+  return <Select options={options} ref={ref} {...props} />;
+});
+
+export default SelectBox;
