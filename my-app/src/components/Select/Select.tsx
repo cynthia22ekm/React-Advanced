2a8622597fd8941417333f40f6b439ac6f92e141
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-22 18:37:59 +0200

    Added onChange for select

--- my-app/src/components/Select/Select.tsx
+++ my-app/src/components/Select/Select.tsx
@@ -1,16 +1,33 @@
 import { ForwardedRef, forwardRef } from "react";
 import Select, { SelectInstance } from "react-select";
 
+export type SelectedItemType = {
+  value: string;
+  label: string;
+};
+
 export type SelectProps = {
-  options: { value: string; label: string }[];
+  value?: string;
+  options: SelectedItemType[];
   ref?: ForwardedRef<SelectInstance>;
+  onChange: (selectedItem: SelectedItemType) => void;
 };
 
 const SelectBox: React.FC<SelectProps> = forwardRef<
   SelectInstance,
   SelectProps
->(({ options, ...props }, ref) => {
-  return <Select options={options} ref={ref} {...props} />;
+>(({ value, options, onChange, ...props }, ref) => {
+  return (
+    <Select
+      options={options}
+      value={
+        options.find((option) => option.value === value) as SelectedItemType
+      }
+      ref={ref}
+      onChange={(newValue: unknown) => onChange(newValue as SelectedItemType)}
+      {...props}
+    />
+  );
 });
 
 export default SelectBox;
