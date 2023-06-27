5c0a4e4872c69491f6ace116f5b09efb6de814c0
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-27 12:57:26 +0200

    Styled button component

--- my-app/src/components/Button/Button.tsx
+++ my-app/src/components/Button/Button.tsx
@@ -4,25 +4,27 @@ export type ButtonSize = "small" | "medium" | "large";
 export type ButtonType = "submit" | "button" | "reset";
 
 export type ButtonProps = {
-  label: string;
+  label?: string;
+  className?: string;
   type: ButtonType;
   size: ButtonSize;
 };
 
-const StyledButton = styled.button`
-  width: 100px;
+const StyledButton = styled.button<ButtonProps>((props) => {
+  const { size } = props;
+  return `
+  width:${size === "small" ? "100px" : size === "medium" ? "200px" : "300px"} ;
   height: 30px;
   border-radius: 5px;
 `;
+});
 
-const Button: React.FC<ButtonProps> = ({
-  label,
-  type = "button",
-  size = "medium",
-}) => {
+const Button: React.FC<ButtonProps> = ({ label, className, type, size }) => {
   return (
     <div>
-      <StyledButton type={type}>{label}</StyledButton>
+      <StyledButton label={label} className={className} type={type} size={size}>
+        {label}
+      </StyledButton>
     </div>
   );
 };
