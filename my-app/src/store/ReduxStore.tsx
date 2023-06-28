086dd3a459655a90755d2ee6d95af5723f798e3e
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-28 18:19:01 +0200

    Added user slice to redux store

--- my-app/src/store/ReduxStore.tsx
+++ my-app/src/store/ReduxStore.tsx
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "../reduxSlice/CounterSlice";
 import ShoppingCartReducer from "../reduxSlice/ShoppingCarSlice";
 import ProductReducer from "../reduxSlice/ProductSlice";
+import UserReducer from "../reduxSlice/UserSlice";
 import productsApi from "../API/api";
 
 //https://dev.to/ifeanyichima/what-is-createasyncthunk-in-redux--mhe
@@ -12,6 +13,7 @@ export const ReduxStore = configureStore({
     shoppingcart: ShoppingCartReducer,
     [productsApi.reducerPath]: productsApi.reducer,
     productData: ProductReducer,
+    userData: UserReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productsApi.middleware),
