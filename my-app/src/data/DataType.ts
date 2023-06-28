84d5aec3495bb0605f95befbe4be8c5e47f868d2
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-28 09:22:12 +0200

    Renamed the properties

--- my-app/src/data/DataType.ts
+++ my-app/src/data/DataType.ts
@@ -1,9 +1,9 @@
 export type Products = {
   id: number;
   title: string;
-  price: number;
+  purchasePrice: number;
   description: string;
   category: string;
   image: string;
-  rating: { rate: number; count: number };
+  rating: { salesPrice: number; quantity: number };
 };
