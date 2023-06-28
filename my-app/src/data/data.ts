c9992cc489f3969f5ee3356ca5cc05a673cc74a4
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-28 09:22:28 +0200

    Renamed properties

--- my-app/src/data/data.ts
+++ my-app/src/data/data.ts
@@ -9,51 +9,51 @@ export const Data: Products[] = [
   {
     id: 10,
     title: "Banana",
-    price: 1.3,
+    purchasePrice: 1.3,
     category: "Food",
     description:
       "Your perfect pack for everyday use and walks in th…to 15 inches) in the padded sleeve, your everyday",
     image: banana,
-    rating: { rate: 4.1, count: 259 },
+    rating: { salesPrice: 4.1, quantity: 259 },
   },
   {
     id: 20,
     title: "Book",
-    price: 22.3,
+    purchasePrice: 22.3,
     category: "Books",
     description:
       "Your perfect pack for everyday use and walks in th…to 15 inches) in the padded sleeve, your everyday",
     image: book,
-    rating: { rate: 4.1, count: 259 },
+    rating: { salesPrice: 4.1, quantity: 259 },
   },
   {
     id: 30,
     title: "Computer",
-    price: 2200.3,
+    purchasePrice: 2200.3,
     category: "Electronics",
     description:
       "Your perfect pack for everyday use and walks in th…to 15 inches) in the padded sleeve, your everyday",
     image: computer,
-    rating: { rate: 4.1, count: 259 },
+    rating: { salesPrice: 4.1, quantity: 259 },
   },
   {
     id: 40,
     title: "Kids Clothing",
-    price: 220.3,
+    purchasePrice: 220.3,
     category: "Clothing",
     description:
       "Your perfect pack for everyday use and walks in th…to 15 inches) in the padded sleeve, your everyday",
     image: dress,
-    rating: { rate: 4.1, count: 259 },
+    rating: { salesPrice: 4.1, quantity: 259 },
   },
   {
     id: 50,
     title: "Women's Clothing",
-    price: 220.3,
+    purchasePrice: 220.3,
     category: "Clothing",
     description:
       "Your perfect pack for everyday use and walks in th…to 15 inches) in the padded sleeve, your everyday",
     image: womenClothing,
-    rating: { rate: 4.1, count: 259 },
+    rating: { salesPrice: 4.1, quantity: 259 },
   },
 ];
