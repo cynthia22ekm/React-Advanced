4ba2c5135bed3c0e8702acef32526adf7d5d693c
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-30 11:10:36 +0200

    Changed the type of data returned by api

--- my-app/src/API/api.tsx
+++ my-app/src/API/api.tsx
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { ProductType } from "../data/DataType";
+import { APIProductType } from "../data/DataType";
 
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://fakestoreapi.com/" }),
   endpoints: (builder) => ({
-    getProducts: builder.query<ProductType[], void>({
+    getProducts: builder.query<APIProductType[], void>({
       query: () => ({ url: `/Products` }),
     }),
   }),
