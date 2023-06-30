2ff25cfd735f32791e9e22883ed56792efa4a445
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-06-30 11:10:21 +0200

    Adding padding for text

--- my-app/src/pages/Home/Table/PriceCell.tsx
+++ my-app/src/pages/Home/Table/PriceCell.tsx
@@ -1,11 +1,16 @@
 import { CellProps } from "react-table";
+import styled from "styled-components";
 
 type PriceCellProps = {
   cellprops: CellProps<any>;
 };
 
+const StyledCell = styled.div`
+  padding: 10px;
+`;
+
 const PriceCell: React.FC<PriceCellProps> = ({ cellprops: { row } }) => {
-  return <div>{row.original["price"]}</div>;
+  return <StyledCell>{row.original["price"]} </StyledCell>;
 };
 
 export default PriceCell;
