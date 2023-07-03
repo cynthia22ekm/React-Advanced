05cc28165bda175c6a2a2d50c0ab6d7cdaa11d31
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-07-03 13:48:59 +0200

    Implemented global filter in table

--- my-app/src/pages/Product/AddProductTable.tsx
+++ my-app/src/pages/Product/AddProductTable.tsx
@@ -1,7 +1,9 @@
-import { useSortBy, useTable } from "react-table";
+import { useGlobalFilter, useSortBy, useTable } from "react-table";
 import { ProductType } from "../../data/DataType";
 import { columns } from "./Columns";
 import styled from "styled-components";
+import GlobalFilter from "./GlobalFilter";
+import { ChangeEvent } from "react";
 
 export type AddProductTableProps = {
   data: ProductType[];
@@ -22,41 +24,61 @@ const Styledtd = styled.td`
 `;
 
 const AddProductTable: React.FC<AddProductTableProps> = ({ data }) => {
-  const { getTableProps, getTableBodyProps, visibleColumns, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data,
-      },
-      useSortBy
-    );
+  const {
+    visibleColumns,
+    rows,
+    state,
+    setGlobalFilter,
+    getTableProps,
+    getTableBodyProps,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter,
+    useSortBy
+  );
+
+  const { globalFilter } = state;
+
+  const getSearchTextHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setGlobalFilter(event.target.value);
+  };
 
   return (
-    <StyledTable {...getTableProps()}>
-      <thead>
-        <tr>
-          {visibleColumns.map((column) => (
-            <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <Styledtd {...cell.getCellProps()}>
-                    {cell.render("Cell")}
-                  </Styledtd>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </StyledTable>
+    <div>
+      <GlobalFilter
+        globalFilter={globalFilter}
+        onChange={getSearchTextHandler}
+      ></GlobalFilter>
+      <StyledTable {...getTableProps()}>
+        <thead>
+          <tr>
+            {visibleColumns.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <Styledtd {...cell.getCellProps()}>
+                      {cell.render("Cell")}
+                    </Styledtd>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </StyledTable>
+    </div>
   );
 };
 
