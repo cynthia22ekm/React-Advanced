cc0b89e12a3108e45ef8f11ad30ce39b1cd85e2a
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-07-13 16:30:55 +0200

    Added margin for textinput

--- my-app/src/pages/Product/ColumnFilter.tsx
+++ my-app/src/pages/Product/ColumnFilter.tsx
@@ -2,15 +2,20 @@ import { ColumnInstance } from "react-table";
 import { ChangeEvent } from "react";
 import TextInput from "../../components/TextInput/TextInput";
 import { ProductType } from "../../data/DataType";
+import styled from "styled-components";
 
 type ColumnFilterProps = {
   column: ColumnInstance<ProductType>;
 };
 
+const StyledTextInput = styled(TextInput)`
+  margin: 10px;
+`;
+
 const ColumnFilter: React.FC<ColumnFilterProps> = ({ column }) => {
   const { filterValue, setFilter } = column;
   return (
-    <TextInput
+    <StyledTextInput
       inputSize="small"
       value={filterValue ? filterValue : ""}
       onChange={(event: ChangeEvent<HTMLInputElement>) =>
