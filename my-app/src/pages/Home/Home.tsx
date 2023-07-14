16ccfda432324ee42894a24d185e9ad0ce4b9df1
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-07-14 11:10:47 +0200

    Utilised data in store

--- my-app/src/pages/Home/Home.tsx
+++ my-app/src/pages/Home/Home.tsx
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
 import { useGetProductsQuery } from "../../API/api";
 import Navbar from "../../components/Navbar/Navbar";
 import ProductsTable from "./Table/ProductsTable";
+import { RootState } from "../../store/ReduxStore";
 
 const Home: React.FC = () => {
   const { data } = useGetProductsQuery();
+  const productData = useSelector((state: RootState) => {
+    return state.productApiData.productApiData;
+  });
   return (
     <div>
       <Navbar
@@ -16,7 +21,7 @@ const Home: React.FC = () => {
         itemCount={0}
       />
 
-      <ProductsTable data={data ? data : []} />
+      <ProductsTable data={productData ? productData : []} />
     </div>
   );
 };
